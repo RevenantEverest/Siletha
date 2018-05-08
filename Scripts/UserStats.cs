009d6aa0815cb12541f14a6bc581959d6848c9ab
Stefan <stefischer@mail.com>
2018-05-08 12:02:36 -0400

    Base camera and movement scrips, framework for stats

---
+++ Scripts/UserStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserStats : MonoBehaviour {
+
+    public string characterName;
+    public int level;
+
+    public float currentHealth;
+    public float maxHealth;
+    public float currentMana;
+    public float maxMana;
+
+    public float baseAttackPower;
+    public float currentAttackPower;
+    public float baseAttackSpped;
+    public float currentAttackSpeed;
+    public float baseDodge;
+    public float currentDodge;
+    public float baseHitPercent;
+    public float currentHitPercent;
+
+    public float hpRegenTimer;
+    public float hpRegenAmount;
+
+    public float manaRegenTimer;
+    public float manaRegenAmount;
+
+    public float currentXP;
+    public float maxXP;
+
+    public bool isDead;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+}
