8e285254e5f5f8864815a761f3c46b74165c33de
Stefan <stefischer@mail.com>
2018-05-10 18:44:09 -0400

    MMO style attack, restrictions on when player can attack

---
+++ Scripts/EnemyStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+
+	public float currentHealth;
+	public float maxHealth;
+
+	public bool isDead = false;
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+		if(currentHealth <= 0) {
+			currentHealth = 0;
+			isDead = true;
+		}
+	}
+
+	public void RecieveDamage(float dmg) {
+		currentHealth -= dmg;
+
+		print("Damage done => " + dmg);
+		print("Enemy Health => " + currentHealth);
+	}
+}
--- Scripts/UserStats.cs
+++ Scripts/UserStats.cs
@@ -4,40 +4,126 @@ using UnityEngine;
 
 public class UserStats : MonoBehaviour {
 
+    //Character Info
     public string characterName;
     public int level;
     public string userClass;
 
-    public float currentHealth;
+    //Character Stats
+    public float currentHealth;               //Health
     public float maxHealth;
-    public float currentMana;
-    public float maxMana;
-
-    public float baseAttackPower;
-    public float currentAttackPower;
-    public float baseAttackSpped;
-    public float currentAttackSpeed;
-    public float baseDodge;
-    public float currentDodge;
-    public float baseHitPercent;
-    public float currentHitPercent;
-
-    public float hpRegenTimer;
+    public float hpRegenTimer;                //Health Regen
     public float hpRegenAmount;
 
-    public float manaRegenTimer;
+    public float currentMana;                 //Mana
+    public float maxMana;
+    public float manaRegenTimer;              //Mana Regen
     public float manaRegenAmount;
 
-    public float currentXP;
+
+    public float baseAttackPower;             //Attack Power
+    public float currentAttackPower;
+
+    public float baseAttackSpped;             //Attack Speed
+    public float currentAttackSpeed;
+
+    public float baseDodge;                   //Dodge
+    public float currentDodge;
+
+    public float baseHitPercent;              //Hit Percent
+    public float currentHitPercent;
+
+
+    public float currentXP;                   //Experience
     public float maxXP;
 
-    public bool isDead;
+    public bool isDead = false;
+
+    //Selection
+    public GameObject selectedUnit;
+
+    //Current Enemy References
+    public EnemyStats enemyStatsScript;
+
+    //Attack
+    public bool behindEnemy;
+    public bool canAttack;
+
+    //KeyCodes
+    public KeyCode attack;
 
 	void Start () {
 
 	}
 
 	void Update () {
+    if(Input.GetMouseButtonDown(0)) {
+
+      SelectTarget();
+    }
+
+    if(selectedUnit != null) {
+
+      Vector3 toTarget = (selectedUnit.transform.position - transform.position).normalized;
+
+      //Check if player is behind enemy ( Calculate dodge, parry, extra damage, etc. )
+      if(Vector3.Dot(toTarget, selectedUnit.transform.forward) < 0) {
+        behindEnemy = false;
+      }else {
+        behindEnemy = true;
+      }
+
+      //Calculate if the player is facing the enemy and is within attack distance
+      float distance = Vector3.Distance(this.transform.position, selectedUnit.transform.position);
+      Vector3 targetDir = selectedUnit.transform.position - transform.position;
+      Vector3 forward = transform.forward;
+      float angle = Vector3.Angle(targetDir, forward);
+
+      if(angle > 60.0) {
+
+        canAttack = false;
+      }else {
+
+        if(distance < 60) {
+
+          canAttack = true;
+        }else {
+
+          canAttack = false;
+        }
+      }
+
+    }
+
+    if(Input.GetKeyDown(attack)) {
+
+      if(selectedUnit != null && canAttack) {
+
+        BasicAttack();
+      }
+    }
+    
+    //TODO: Ranged Spell Attack
 
 	}
+
+  void SelectTarget() {
+
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+
+    if(Physics.Raycast(ray, out hit, 10000)) {
+
+      if(hit.transform.tag == "enemy") {
+        selectedUnit = hit.transform.gameObject;
+
+        enemyStatsScript = selectedUnit.transform.gameObject.transform.GetComponent<EnemyStats>();
+      }
+    }
+  }
+
+  void BasicAttack() {
+
+    enemyStatsScript.RecieveDamage(10);
+  }
 }
